0b6596eda37eb60ac8588ebcb3e738db05c7ac5c
Yanick van der Korst <yanick.van.der.korst@kpnmail.nl>
2026-01-02 14:23:52 +0100

    v1.1.6

--- db-acf-ui.php
+++ db-acf-ui.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: DB ACF Extension
 Description: Aangepaste ACF interface voor Digitale Bazen
-Version: 1.2.5
+Version: 1.2.6
 Author: Digitale Bazen
 Text Domain: db-acf-ui
 Update URI: bitbucket.org/digitale-bazen/db-acf-extension
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Constants
  * ---------------------------
  */
-define( 'DB_ACF_UI_VERSION', '1.2.5' );
+define( 'DB_ACF_UI_VERSION', '1.2.6' );
 define( 'DB_ACF_UI_MIN_PHP_VERSION', '8.0' );
 
 define( 'DB_ACF_UI_FILE', __FILE__ );
@@ -128,104 +128,22 @@ add_filter('pre_set_site_transient_update_plugins', function($transient) {
 });
 
 
-
-
-
-/**
- * Forceer vaste pluginmapnaam bij updates én migreer 'active_plugins' naar het nieuwe pad.
- */
 add_filter('upgrader_source_selection', function ($source, $remote_source, $upgrader, $hook_extra) {
-
-    // Alleen ingrijpen bij plugin install/update
     if (!isset($hook_extra['type']) || $hook_extra['type'] !== 'plugin') {
         return $source;
     }
 
     global $wp_filesystem;
-    if (!$wp_filesystem) {
-        return $source;
-    }
-
-    // ---- Stel hier je vaste map- en bestandsnaam in ----
     $desired_folder_name = 'db-acf-extension';
-    $main_file_name      = 'db-acf-ui.php'; // hoofd-pluginbestand
-    $desired_basename    = $desired_folder_name . '/' . $main_file_name;
+    $new_source = trailingslashit($remote_source) . $desired_folder_name . '/';
 
-    // Huidige (oude) basename zoals WP hem nu kent (kan de hash-map zijn)
-    $old_basename = plugin_basename(DB_ACF_UI_FILE);
-
-    // 1) Hernoem de uitgepakte bronmap BINNEN $remote_source naar de vaste naam
-    //    zodat de upgrader de uiteindelijke map als wp-content/plugins/db-acf-extension neerzet.
-    if (basename($source) !== $desired_folder_name) {
-
-        $new_source = trailingslashit($remote_source) . $desired_folder_name . '/';
-
-        // Ruim eventueel bestaande map op die naam in de unpack-temp op
-        if ($wp_filesystem->is_dir($new_source)) {
-            $wp_filesystem->delete($new_source, true);
-        }
-
-        // Verplaats/rename de uitgepakte bronmap naar de vaste naam
-        $moved = $wp_filesystem->move($source, $new_source, true);
-
-        if ($moved) {
-            // Zeer belangrijk: vanaf hier moet de upgrader dit nieuwe pad gebruiken
-            $source = $new_source;
-        }
-        // Als move faalt, laten we $source ongemoeid (fallback)
+    if ($wp_filesystem->is_dir($new_source)) {
+        $wp_filesystem->delete($new_source, true);
     }
 
-    // 2) Migreer de opgeslagen pluginpaden naar de nieuwe basename, zodat WP hem actief houdt.
-    //    Dit vangt het scenario waarin de plugin eerder onder een hash-map actief was.
-
-    // a) Gewone (niet-multisite) activatie
-    $active = get_option('active_plugins', []);
-    $changed = false;
-
-    foreach ($active as $i => $path) {
-        // Exact oude waarde, of een pad dat op de hash-variant lijkt
-        if ($path === $old_basename
-            || preg_match('#^digitale-bazen-db-acf-extension-[^/]+/' . preg_quote($main_file_name, '#') . '$#', $path)
-            || preg_match('#^db-acf-extension-[^/]+/' . preg_quote($main_file_name, '#') . '$#', $path) // voor andere prefixen
-        ) {
-            $active[$i] = $desired_basename;
-            $changed = true;
-        }
+    if ($wp_filesystem->move($source, $new_source, true)) {
+        return $new_source;
     }
 
-    if ($changed) {
-        update_option('active_plugins', $active);
-    }
-
-    // b) Multisite: netwerkactivatie
-    if (is_multisite()) {
-        $sitewide = get_site_option('active_sitewide_plugins', []);
-        $sw_changed = false;
-
-        // Exact key match
-        if (isset($sitewide[$old_basename])) {
-            $sitewide[$desired_basename] = $sitewide[$old_basename];
-            unset($sitewide[$old_basename]);
-            $sw_changed = true;
-        } else {
-            // Hash-variant zoeken
-            foreach (array_keys($sitewide) as $key) {
-                if (preg_match('#^digitale-bazen-db-acf-extension-[^/]+/' . preg_quote($main_file_name, '#') . '$#', $key)
-                    || preg_match('#^db-acf-extension-[^/]+/' . preg_quote($main_file_name, '#') . '$#', $key)
-                ) {
-                    $sitewide[$desired_basename] = $sitewide[$key];
-                    unset($sitewide[$key]);
-                    $sw_changed = true;
-                }
-            }
-        }
-
-        if ($sw_changed) {
-            update_site_option('active_sitewide_plugins', $sitewide);
-        }
-    }
-
-    // Retourneer (mogelijk) aangepast source-pad in de unpack-temp
     return $source;
-
 }, 99, 4);
