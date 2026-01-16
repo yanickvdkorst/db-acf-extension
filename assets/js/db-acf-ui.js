a8909cef6a002a0a7c1dfc3438e907208e866a7f
Yanick van der Korst <yanick.van.der.korst@kpnmail.nl>
2026-01-16 16:01:14 +0100

    Removed bug of more disable buttons per card

--- assets/js/db-acf-ui.js
+++ assets/js/db-acf-ui.js
@@ -1,48 +1,48 @@
-(function($) {
-
+(function ($) {
   const ACFDB = {
-
     modals: [],
 
     init() {
       ACFDB.removeSinglePreviewModal();
       ACFDB.addPreviewModalLinkMarkup();
 
+      
       acf.addAction('load_field/type=flexible_content', function(field) {
         field.$el.find('.acf-flexible-content:first > .values > .layout:not(.fc-modal)').each(function() {
           ACFDB.addModal($(this));
         });
       });
 
-      acf.addAction('after_duplicate', function($clone, $el) {
-        if ($el.is('.layout')) ACFDB.addModal($el);
+
+      acf.addAction("after_duplicate", function ($clone, $el) {
+        if ($el.is(".layout")) ACFDB.addModal($el);
       });
 
-      acf.addAction('append', function($el) {
-        if ($el.is('.layout')) ACFDB.addModal($el);
+      acf.addAction("append", function ($el) {
+        if ($el.is(".layout")) ACFDB.addModal($el);
       });
 
-      acf.addAction('invalid_field', function(field) {
+      acf.addAction("invalid_field", function (field) {
         ACFDB.invalidField(field.$el);
       });
 
-      acf.addAction('valid_field', function(field) {
+      acf.addAction("valid_field", function (field) {
         ACFDB.validField(field.$el);
       });
 
-      $(document).keyup(function(e) {
-        if (e.keyCode === 27 && $('body').hasClass('acf-modal-open')) {
+      $(document).keyup(function (e) {
+        if (e.keyCode === 27 && $("body").hasClass("acf-modal-open")) {
           ACFDB.close();
         }
       });
     },
 
     removeSinglePreviewModal() {
-      const flexibleContentField = acf.getField('acf-field-flexible-content');
-      flexibleContentField._open = function() {
-        const $popup = $(this.$el.children('.tmpl-popup').html());
-        if ($popup.find('a').length === 1) {
-          flexibleContentField.add($popup.find('a').attr('data-layout'));
+      const flexibleContentField = acf.getField("acf-field-flexible-content");
+      flexibleContentField._open = function () {
+        const $popup = $(this.$el.children(".tmpl-popup").html());
+        if ($popup.find("a").length === 1) {
+          flexibleContentField.add($popup.find("a").attr("data-layout"));
           return false;
         }
         return flexibleContentField.apply(this, arguments);
@@ -50,23 +50,29 @@
     },
 
     addPreviewModalLinkMarkup() {
-      $('body').on('click', 'a[data-name="add-layout"]', function() {
-        $('.acf-fc-popup a').each(function() {
-          const html = '<div class="acf-fc-popup-label">' + $(this).html() + '</div><div class="acf-fc-popup-image"></div>';
+      $("body").on("click", 'a[data-name="add-layout"]', function () {
+        // Update HTML van popup items
+        $(".acf-fc-popup a").each(function () {
+          const html =
+            '<div class="acf-fc-popup-label">' +
+            $(this).html() +
+            '</div><div class="acf-fc-popup-image"></div>';
           $(this).html(html);
         });
       });
     },
 
     addModal($layout) {
-      $layout.addClass('fc-modal').removeClass('-collapsed');
+      $layout.addClass("fc-modal").removeClass("-collapsed");
 
-      const $header = $layout.find('> .acf-fc-layout-actions-wrap');
+      const $header = $layout.find("> .acf-fc-layout-actions-wrap");
       if (!$header.length) return;
 
-      let $controls = $header.find('> .acf-fc-layout-controls');
+      let $controls = $header.find("> .acf-fc-layout-controls");
       if (!$controls.length) {
-        $controls = $('<div class="acf-fc-layout-controls"></div>').appendTo($header);
+        $controls = $('<div class="acf-fc-layout-controls"></div>').appendTo(
+          $header
+        );
       }
 
       // remove any old pencil duplicates
@@ -76,66 +82,85 @@
       const $edit = $(
         '<a class="acf-js-tooltip" href="#" data-event="edit-layout" title="Bewerk layout">' +
           '<span class="acf-icon -pencil-alt"></span>' +
-        '</a>'
+          "</a>"
       );
-      $edit.on('click.acfdb', function(e) {
+      $edit.on("click.acfdb", function (e) {
         e.preventDefault();
         e.stopPropagation();
         ACFDB.open.call(this);
       });
       $controls.prepend($edit);
 
-// --- Voeg knop toe om layout te toggle ---
-const $toggleBtn = $(
-    '<a href="#" class="acf-layout-toggle-btn acf-js-tooltip" title="Toggle layout" data-name="toggle-layout">' + 
-      '<span class="acf-icon -toggle-alt"></span>'
-    + '</a>'
-);
-$edit.after($toggleBtn);
+      // remove any old toggle duplicates
+      $controls.find(".acf-layout-toggle-btn").remove();
 
-$toggleBtn.on('click', function(e){
-    e.preventDefault();
-    
-    const $btn = $(this);
-    const $icon = $btn.find('span');
-    const $layout = $btn.closest('.layout');
-    const fcField = acf.getInstance($layout.closest('.acf-field-flexible-content'));
-    if (!fcField) return;
+      // --- Voeg knop toe om layout te toggle ---
+      const $toggleBtn = $(
+        '<a href="#" class="acf-layout-toggle-btn acf-js-tooltip" title="Toggle layout" data-name="toggle-layout">' +
+          '<span class="acf-icon -toggle-alt"></span>' +
+          "</a>"
+      );
+      $edit.after($toggleBtn);
 
-    // ACF toggle
-    fcField.onClickToggleLayout(null, $layout);
+      $toggleBtn.on("click", function (e) {
+        e.preventDefault();
 
-    // icon class toggle
-    $icon.toggleClass('disabled');
-});
+        const $btn = $(this);
+        const $icon = $btn.find("span");
+        const $layout = $btn.closest(".layout");
+        const fcField = acf.getInstance(
+          $layout.closest(".acf-field-flexible-content")
+        );
+        if (!fcField) return;
+
+        // ACF toggle
+        fcField.onClickToggleLayout(null, $layout);
+
+        // icon class toggle
+        $icon.toggleClass("disabled");
+      });
       // header click opent modal, behalve als pencil wordt geklikt
-      $header.off('click.acfdb').on('click.acfdb', function(e) {
-        if ($(e.target).closest('.acf-fc-layout-controls').length) return;
+      $header.off("click.acfdb").on("click.acfdb", function (e) {
+        if ($(e.target).closest(".acf-fc-layout-controls").length) return;
         ACFDB.open.call(this);
       });
 
       // fields container
-      if ($layout.find('> .acf-fields, > .acf-table').length === 0) {
+      if ($layout.find("> .acf-fields, > .acf-table").length === 0) {
         $layout.append('<div class="acf-fields"></div>');
       }
-      $layout.find('> .acf-fields, > .acf-table').wrapAll('<div class="acf-fc-modal-content" />');
+      $layout
+        .find("> .acf-fields, > .acf-table")
+        .wrapAll('<div class="acf-fc-modal-content" />');
 
       // dubbelklik header opent modal
-      $layout.find('> .acf-fc-layout-handle').off('dblclick').on('dblclick', ACFDB.open);
+      $layout
+        .find("> .acf-fc-layout-handle")
+        .off("dblclick")
+        .on("dblclick", ACFDB.open);
     },
 
     open() {
-      const $layout = $(this).closest('.layout');
-      const caption = $layout.find('.acf-fc-layout-title strong').first().text();
+      const $layout = $(this).closest(".layout");
+      const caption = $layout
+        .find(".acf-fc-layout-title strong")
+        .first()
+        .text();
 
-      const closeBtn = $('<a class="dashicons dashicons-no -cancel" />').on('click', ACFDB.close);
-      $layout.find('> .acf-fc-modal-title').html(caption).append(closeBtn);
+      const closeBtn = $('<a class="dashicons dashicons-no -cancel" />').on(
+        "click",
+        ACFDB.close
+      );
+      $layout.find("> .acf-fc-modal-title").html(caption).append(closeBtn);
 
-      $layout.addClass('-modal');
+      $layout.addClass("-modal");
       ACFDB.modals.push($layout);
 
       // disable dragging while modal is open
-      $layout.closest('.acf-flexible-content').find('.acf-fc-layout-handle').css('pointer-events', 'none');
+      $layout
+        .closest(".acf-flexible-content")
+        .find(".acf-fc-layout-handle")
+        .css("pointer-events", "none");
 
       ACFDB.overlay(true);
     },
@@ -144,57 +169,62 @@ $toggleBtn.on('click', function(e){
       const $layout = ACFDB.modals.pop();
       if (!$layout) return;
 
-      const fc = $layout.parents('.acf-field-flexible-content:first');
+      const fc = $layout.parents(".acf-field-flexible-content:first");
       const field = acf.getInstance(fc);
-      if (field && field.closeLayout) field.closeLayout(field.$layout($layout.index()));
+      if (field && field.closeLayout)
+        field.closeLayout(field.$layout($layout.index()));
 
-      $layout.find('> .acf-fc-modal-title').html('');
-      $layout.removeClass('-modal').css('visibility', '');
-      $layout.addClass('-highlight-closed');
+      $layout.find("> .acf-fc-modal-title").html("");
+      $layout.removeClass("-modal").css("visibility", "");
+      $layout.addClass("-highlight-closed");
 
       // re-enable dragging
-      $layout.closest('.acf-flexible-content').find('.acf-fc-layout-handle').css('pointer-events', '');
+      $layout
+        .closest(".acf-flexible-content")
+        .find(".acf-fc-layout-handle")
+        .css("pointer-events", "");
 
-      setTimeout(() => $layout.removeClass('-highlight-closed'), 750);
+      setTimeout(() => $layout.removeClass("-highlight-closed"), 750);
       ACFDB.overlay(false);
     },
 
     overlay(show) {
-      if (show && !$('body').hasClass('acf-modal-open')) {
-        $('<div id="acf-flexible-content-modal-overlay" />').on('click', ACFDB.close).appendTo('body');
-        $('body').addClass('acf-modal-open');
+      if (show && !$("body").hasClass("acf-modal-open")) {
+        $('<div id="acf-flexible-content-modal-overlay" />')
+          .on("click", ACFDB.close)
+          .appendTo("body");
+        $("body").addClass("acf-modal-open");
       } else if (!show && ACFDB.modals.length === 0) {
-        $('#acf-flexible-content-modal-overlay').remove();
-        $('body').removeClass('acf-modal-open');
+        $("#acf-flexible-content-modal-overlay").remove();
+        $("body").removeClass("acf-modal-open");
       }
       ACFDB.refresh();
     },
 
     refresh() {
-      $.each(ACFDB.modals, function() {
-        $(this).css('visibility', 'hidden').removeClass('-animate');
+      $.each(ACFDB.modals, function () {
+        $(this).css("visibility", "hidden").removeClass("-animate");
       });
       const index = ACFDB.modals.length - 1;
       if (index in ACFDB.modals) {
-        ACFDB.modals[index].css('visibility', 'visible').addClass('-animate');
+        ACFDB.modals[index].css("visibility", "visible").addClass("-animate");
       }
     },
 
     invalidField($el) {
-      $el.parents('.layout').addClass('layout-error-messages');
+      $el.parents(".layout").addClass("layout-error-messages");
     },
 
     validField($el) {
-      $el.parents('.layout').each(function() {
+      $el.parents(".layout").each(function () {
         const $layout = $(this);
-        if (!$layout.find('.acf-error').length) $layout.removeClass('layout-error-messages');
+        if (!$layout.find(".acf-error").length)
+          $layout.removeClass("layout-error-messages");
       });
-    }
-
+    },
   };
 
-  $(function() {
+  $(function () {
     ACFDB.init();
   });
-
 })(jQuery);
